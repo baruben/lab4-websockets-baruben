dbf868803afa57f6a3349e0436c4c7b266e5a1c3
baruben <873680@unizar.es>
2025-10-30 17:16:53 +0100

    Implement Session Management and Broadcast

--- src/main/kotlin/websockets/ElizaServer.kt
+++ src/main/kotlin/websockets/ElizaServer.kt
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.socket.server.standard.ServerEndpointExporter
 import java.util.Locale
 import java.util.Scanner
+import java.util.concurrent.CopyOnWriteArraySet
 
 @SpringBootApplication
 class Application
@@ -111,6 +112,79 @@ class ElizaEndpoint {
         } else {
             session.close(CloseReason(CloseCodes.NORMAL_CLOSURE, "Alright then, goodbye!"))
         }
+    }
+
+    @OnError
+    fun onError(
+        session: Session,
+        errorReason: Throwable,
+    ) {
+        logger.error(errorReason) { "Session ${session.id} closed because of ${errorReason.javaClass.name}" }
+    }
+}
+
+@ServerEndpoint("/broadcast")
+@Component
+class BroadcastEndpoint {
+    companion object {
+        // Shared among all instances of this endpoint
+        private val sessions: MutableSet<Session> = CopyOnWriteArraySet()
+    }
+
+    private fun broadcast(message: String) {
+        sessions.forEach { s ->
+            logger.info { "Sending message ... Session ${s.id}" }
+            runCatching {
+                if (s.isOpen) {
+                    with(s.basicRemote) {
+                        sendTextSafe(message)
+                    }
+                }
+            }.onFailure {
+                logger.error(it) { "Error while sending message to ${s.id}" }
+            }
+        }
+    }
+
+    /**
+     * Successful connection
+     *
+     * @param session
+     */
+    @OnOpen
+    fun onOpen(session: Session) {
+        logger.info { "Server Connected ... Session ${session.id}" }
+        sessions.add(session)
+        broadcast("New user ${session.id} connected")
+    }
+
+    /**
+     * Connection closure
+     *
+     * @param session
+     */
+    @OnClose
+    fun onClose(
+        session: Session,
+        closeReason: CloseReason,
+    ) {
+        logger.info { "Session ${session.id} closed because of $closeReason" }
+        sessions.remove(session)
+        broadcast("User ${session.id} disconnected")
+    }
+
+    /**
+     * Message received
+     *
+     * @param message
+     */
+    @OnMessage
+    fun onMsg(
+        message: String,
+        session: Session,
+    ) {
+        logger.info { "New Message ... Session ${session.id} ... $message"}
+        broadcast("[${session.id}]: $message")
     }
 
     @OnError
