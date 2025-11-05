c55b668f06c39c640044325f0b780520ef86f30a
baruben <873680@unizar.es>
2025-11-05 10:38:47 +0100

    Add assert in tests

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -69,6 +69,11 @@ class ElizaServerTest {
         // 4. COMPLETE assertEquals(XXX, list[XXX])
         // The first message should always be the greeting.
         assertEquals("The doctor is in.", list[0])
+        // The fourth message should be a doctor-style reponse.
+        assertTrue(
+            list[3].matches(Regex(".*(sorry|How long|normal to be|enjoy being).*")) && list[3].contains("feeling sad"),
+            "Expected a doctor-style response, but got: ${list[3]}"
+        )
     }
 }
 
